005d23a49be0708330cca362afb52336cfa9d526
shouta-f <kabotyapu@gmail.com>
2020-06-15 23:19:19 +0900

    ろぐいん画面作成

--- account/views.py
+++ account/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('brog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'account/signup.html', {'form': form})
